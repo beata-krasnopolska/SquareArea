ec74019f7349c59ab792b880b41d4f5d1baf01bd
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-07-27 14:57:38 +0200

    Add project files.

---
+++ SquareArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square[] squares = new Square[10];
+            for(int i =0; i<10; i++)
+            {
+                squares[i] = new Square(i + 1);
+                Console.WriteLine("The side: {0} , the area {1}", squares[i].GetSide, squares[i].GetArea);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ SquareArea/Square.cs
@@ -0,0 +1,34 @@
+﻿namespace SquareArea
+{
+    class Square
+    {
+        private int area;
+        private int length;
+
+        public Square (int length)
+        {
+            this.length = length;
+            SquareArea();
+        }
+        private void SquareArea()
+        {
+            area = length * length;           
+        }
+        public int GetSide
+        {
+            get
+            {
+                return length;
+            }           
+        }
+        public int GetArea
+        {
+            get
+            {
+                return area;
+            }
+        }
+
+
+    }
+}
